659e9f06e1ca029cb2dad26955824d1e17e531cc
Codeer12 <39409827+Codeer12@users.noreply.github.com>
2023-07-31 05:26:10 -0400

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,39 @@
+var page_count = 0;
+function previousPage() {
+
+	var viewer1 = document.getElementById("viewer1");
+	
+	var viewer1_link = viewer1.getAttribute("src");
+	
+	
+	if (page_count == 0) {
+		var errorMessage = document.getElementById("error");
+		errorMessage.innerHTML = "First page reached";
+	} else {
+		page_count = page_count - 1;
+
+		viewer1_link = "./issue1/" + page_count + ".png";
+		
+		viewer1.setAttribute("src",viewer1_link);
+		}
+		
+}
+
+function nextPage() {
+	var viewer1 = document.getElementById("viewer1");
+	
+	var viewer1_link = viewer1.getAttribute("src");
+	
+	
+	if (page_count == 31) {
+		var errorMessage = document.getElementById("error");
+		errorMessage.innerHTML = "Last page reached";
+	} else {
+		page_count = page_count + 1;
+
+
+		viewer1_link = "./issue1/" + page_count + ".png";
+		
+		viewer1.setAttribute("src",viewer1_link);
+	}
+}
