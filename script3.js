45d250af8a2f78e1368e75aa9d0622a3d951b8ea
Codeer12 <39409827+Codeer12@users.noreply.github.com>
2023-07-31 05:36:59 -0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,12 +1,13 @@
 var a;
-function myfunction() {
+function myFunction() {
          if (navigator.userAgent.match(/Android/i)
          || navigator.userAgent.match(/iPhone/i)
          || navigator.userAgent.match(/iPad/i)
-         || navigator.userAgent.match(/iPod/i)
+         || navigator.userAgent.match(/iPod/i))
          {
           a = true ;
           window.location.href = '/reader-mobile.html';
          } else {
             a = false ;
          }
+}
