8f93b44121591ab8f68f7a90f95c8e8c14b19960
Codeer12 <39409827+Codeer12@users.noreply.github.com>
2023-08-01 01:02:56 -0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -3,7 +3,7 @@ function myFunction() {
          if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i))
          {
           a = true ;
-          window.location.href = '/reader-mobile.html';
+          window.location.href = 'https://www.canva.com/design/DAFoW0beFgQ/Zq2f7CaZfF44VrLboZC8fQ/view?utm_content=DAFoW0beFgQ&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink';
          } else {
             a = false ;
          }
