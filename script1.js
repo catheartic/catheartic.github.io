05774479ed5dc4e60a7ee406968dde1df31683f8
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-09-10 18:49:38 -0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,11 +1,9 @@
 var page_count = 0;
 function previousPage() {
 
-	var viewer1 = document.getElementById("viewer1");
-	var viewer2 = document.getElementById("viewer2");
+	var viewer1 = document.getElementById("viewer1")
 	
 	var viewer1_link = viewer1.getAttribute("src");
-	var viewer2_link = viewer2.getAttribute("src");
 	
 	
 	if (page_count == 0) {
@@ -14,23 +12,19 @@ function previousPage() {
 	} else {
 		page_count = page_count - 1;
 		
-		var page_count_2 = page_count + 1;
 
 		viewer1_link = "./issue1/" + page_count + ".png";
-		viewer2_link = "./issue1/" + page_count_2 + ".png";
 		
 		viewer1.setAttribute("src",viewer1_link);
-		viewer2.setAttribute("src",viewer2_link);
 		}
 		
 }
 
 function nextPage() {
 	var viewer1 = document.getElementById("viewer1");
-	var viewer2 = document.getElementById("viewer2");
+
 	
 	var viewer1_link = viewer1.getAttribute("src");
-	var viewer2_link = viewer2.getAttribute("src");
 	
 	
 	if (page_count == 31) {
@@ -39,12 +33,10 @@ function nextPage() {
 	} else {
 		page_count = page_count + 1;
 		
-		var page_count_2 = page_count + 1;
+
 
 		viewer1_link = "./issue1/" + page_count + ".png";
-		viewer2_link = "./issue1/" + page_count_2 + ".png";
 		
 		viewer1.setAttribute("src",viewer1_link);
-		viewer2.setAttribute("src",viewer2_link);
 	}
 }
